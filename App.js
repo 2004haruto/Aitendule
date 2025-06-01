9f72a802129b509cf85821ce5d9b545ac197b6e3
haruto <2230033@ecc.ac.jp>
2025-06-01 22:49:01 +0900

    test

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ export default function App() {
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Calendar" component={CalendarScreen} />
-        {/* 後で他の画面も追加aa */}
+        {/* 後で他の画面も追加*/}
       </Stack.Navigator>
     </NavigationContainer>
   );
